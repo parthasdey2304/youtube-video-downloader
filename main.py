5722b062408ca3fba4ccaf5e6f076144d901e391
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-04-27 07:24:29 -0400

    main.py the code for the youtube video downloader has been udpated

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ yt = YouTube(url)
 stream = yt.streams.get_highest_resolution()
 
 print("Downloading....")
-stream.download(filename="video1.mp4")
+
+filename = input("Enter the name with which you want to save the video, without file extension : ")
+stream.download(filename="f{filename}.mp4")
 print("Download completed")
 os.system("vlc video1.mp4")
